b465f39f176b017c25631d4d3ba93580f423fe18
Andreas Keye <andreas.keye@gmail.com>
2024-04-02 17:02:16 +0200

    Umbenennungen

--- _/editVault.sh
+++ _/editVault.sh
@@ -1,8 +1,13 @@
+  export EDITOR=nano
+
+  clear
+
 # Create vault
 # ansible-vault create group_vars/database/vault
+# ansible-vault create group_vars/server/vault
 # Edit vault
   ansible-vault edit group_vars/database/vault
-  ansible-vault edit group_vars/localgroup/vault
+  ansible-vault edit group_vars/server/vault
 # Test variables
   ansible -m debug -a 'var=hostvars[inventory_hostname]' database
-  ansible -m debug -a 'var=hostvars[inventory_hostname]' localgroup
+  ansible -m debug -a 'var=hostvars[inventory_hostname]' server
